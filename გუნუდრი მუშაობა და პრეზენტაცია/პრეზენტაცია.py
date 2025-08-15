f56ad582b0ec54151effa8bf80c8748f9e01873f
dachimagaria <dachiarkania023@gmail.com>
2025-08-15 03:46:38 -0700

    Update პრეზენტაცია.py

--- გუნუდრი მუშაობა და პრეზენტაცია/პრეზენტაცია.py
+++ გუნუდრი მუშაობა და პრეზენტაცია/პრეზენტაცია.py
@@ -1,10 +1,8 @@
 username = input("enter username: ")
 password = input("Enter password: ")
-Balance = 10
-valute = 0
-#valute = 0 იგულისხმება ლარი
-#valute = 1 იგულისხმება დოლარი
-#valute = 2 იგულისხმება ევრო
+Balance_gel = 10
+Balance_eur = 0
+Balance_usd = 0
 # 1 usd = 2.69 gel
 # 1 usd = 0.86 eur
 # 1 gel = 0.32 eur
@@ -15,10 +13,18 @@ if password == "1234" and username == "dachi":
          question = input("What do you want to do? ")
          if question == "log off":
               break
-         if question == "deposit":
-              money = int(input("How much money do you want to deposit(TYPE NUMBERS ONLY)?: "))
-              Balance = Balance + money
-              
+         valute = input("Witch valute do you want (usd gel eur) ")
+     
+         if valute == "gel":
+              print()
+                 
+             
+              if question =="deposit":
+                    money = int(input("How much money do you want to deposit(TYPE NUMBERS ONLY)?: "))
+              Balance_gel = Balance_gel + money
+                 
+               
+               
          if question == "pay":
               payment = int(input("How much money do you want to pay?"))
               if payment <= Balance:
@@ -28,15 +34,8 @@ if password == "1234" and username == "dachi":
                    print("You dont have enough money!(boo hoo)")
          #if question == "conversion":
               
-
 else :
      print("Password or username is incorrect, Try again!")
-
-
-
-
-
-
 
 
 
