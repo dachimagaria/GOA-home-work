d26474c9e7758063f1bfb7b062e1f254b81b0796
dachimagaria <dachiarkania023@gmail.com>
2025-08-16 03:00:54 -0700

    Update პრეზენტაცია.py

--- გუნუდრი მუშაობა და პრეზენტაცია/პრეზენტაცია.py
+++ გუნუდრი მუშაობა და პრეზენტაცია/პრეზენტაცია.py
@@ -15,29 +15,43 @@ if password == "1234" and username == "dachi":
               break
          if question =="deposit":
                valute = input("Witch valute do you want (usd gel eur) ")
-               money = int(input("How much money do you want to deposit(TYPE NUMBERS ONLY THIS IS GEL)?: "))
+               money = int(input("How much money do you want to deposit(TYPE NUMBERS ONLY)?: "))
                if valute == "gel":
                     Balance_gel = Balance_gel + money
+                    print("Deposit sucsessful your balance is", Balance_gel )
                if valute == "usd":
                     money = money*0.37
                     Balance_gel = Balance_gel + money
+                    print("Deposit sucsessful your balance is", Balance_gel )
                if valute == "eur":
                     money = money*0.32
                     Balance_gel = Balance_gel + money
+                    print("Deposit sucsessful your balance is", Balance_gel )
          if question == "pay":
+              where = input("Witch valute do you want to pay (usd gel eur) ")
               payment = int(input("How much money do you want to pay?"))
-              if payment <= Balance:
-                   print("Payment succsesful!")
-                   Balance = Balance - payment
-              else:
-                   print("You dont have enough money!(boo hoo)")
+              if where == "gel":
+                   if payment <= Balance_gel :
+                        Balance_gel = Balance_gel - payment
+                        print("payment sucsessful!")
+                   else:
+                        print("payment failed not enough gel")
+              if where == "usd":
+                    if payment <= Balance_usd :
+                        Balance_usd = Balance_usd - payment
+                        print("payment sucsessful!")
+                    else:
+                        print("payment failed not enough usd")
+              if where == "eur":
+                   if payment <= Balance_eur :
+                        Balance_eur = Balance_eur - payment
+                        print("payment sucsessful!")
+                   else:
+                        print("payment failed not enough eur")   
          #if question == "conversion":
               
 else :
      print("Password or username is incorrect, Try again!")
-
-
-
 
 
 
