6bb31a77f5a5da670633b4c50b5fe0b851e3f0e2
dachimagaria <dachiarkania023@gmail.com>
2025-08-30 11:41:36 -0700

    Update cw.py

--- day18/cw/cw.py
+++ day18/cw/cw.py
@@ -21,8 +21,18 @@ else:
 
 
 
+month = int(input("Enter a number from 1 to 12: "))
 
-
+if month in (12, 1, 2):
+    print("Winter")
+elif month in (3, 4, 5):
+    print("Spring")
+elif month in (6, 7, 8):
+    print("Summer")
+elif month in (9, 10, 11):
+    print("Fall")
+else:
+    print("Invalid number!")
 
 
 
