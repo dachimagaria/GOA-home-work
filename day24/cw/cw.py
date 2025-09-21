471f9711734943adbf7d9885dbdae271a07f1483
dachimagaria <dachiarkania023@gmail.com>
2025-09-20 22:47:36 -0700

    1

---
+++ day24/cw/cw.py
@@ -0,0 +1,58 @@
+
+names = ["ანა", "გიორგი", "მარიამი", "ლუკა", "ნინო", "დავითი", "სოფო"]
+
+print(names[-1])
+print(names[-2])
+print(names[-3])
+print(names[-4])
+print(names[-5])
+print(names[-6])
+print(names[-7])
+
+
+
+
+
+name1 = "ანა"
+name2 = "გიორგი"
+name3 = "ნინო"
+name4 = "ლუკა"
+name5 = "სოფო"
+
+
+
+
+
+names = [name1, name2, name3, name4, name5]
+
+
+print(names[-1])
+
+
+
+
+
+
+
+
+
+name = "irakli"
+print(name[3])   
+print(name[-3])  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
