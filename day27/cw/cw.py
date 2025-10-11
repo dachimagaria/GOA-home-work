1ae87d0d9cfa68c64495a817becb902cc52ed16f
dachimagaria <dachiarkania023@gmail.com>
2025-10-11 12:37:25 -0700

    1

---
+++ day27/cw/cw.py
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+last_numbers =numbers[5 : 10]
+print(last_numbers)
+
+
+
+
+numbers2 =[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+დაჭრილი_რიცხვები =numbers2[-20:-9]
+
+print(numbers2)
+
+
+
+
+
+
+
+
+
+
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+num = int(input("შეიყვანე რიცხვი: "))
+
+
+print(numbers[2:num])
+
+
+
+
+
+
