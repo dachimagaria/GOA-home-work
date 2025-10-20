b25f359ebe16331d743da6fe19692db3a788eb53
dachimagaria <dachiarkania023@gmail.com>
2025-10-20 04:19:47 -0700

    1

---
+++ day31/cw/cw.py
@@ -0,0 +1,29 @@
+
+
+
+list1 = ["ina", "givi", "nika", "daviti", "ia", "lizi"]
+
+
+list1[0:2] = ["irina", "milana", "kira", "mate"]
+
+list1[-2:] = ["gia", "emzari", "xvicha"]
+
+print(list1)
+
+
+
+
+
+
+
+
+
+num = int(input("შეიყვანე რიცხვი: "))
+
+
+
+if num % 2 == 0:
+    print("EVEN")
+else:
+    print("ODD")
+
