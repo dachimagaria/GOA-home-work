73a4ad2f4b59046512bf29caa1b2605387e8511a
dachimagaria <dachiarkania023@gmail.com>
2025-10-25 04:55:17 -0700

    1

--- day29/hw/hw.py
+++ day29/hw/hw.py
@@ -8,27 +8,30 @@
 
 
 
-# 1)მოცემულია სტრინგი "PythonProgramming".
-# ამოიღე პირველი 6 სიმბოლო და დაბეჭდე გამოიყენეთ slicing
+text = "PythonProgramming"
+print(text[:6])   
 
-# 2)მოცემულია სია numbers = [10, 20, 30, 40, 50, 60, 70].
-# ამოიღე მხოლოდ შუა 3 ელემენტი და დაბეჭდე გამოიყენეთ slicing (მინუს ინდექსებითაც)
+# 2)
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print(numbers[2:5])     
+print(numbers[-5:-2])   
 
-# 3)მოცემულია სტრინგი "HelloWorld".
-# დაბეჭდეთ Hello ტერმინალში slicing ის გამოყენებით (მინუს ინდექსებითაც)
+# 3)
+word = "HelloWorld"
+print(word[:5])      
+print(word[:-5])     
 
-# 3)მოცემულია სია letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'].
-# დაბეჭდე ყოველ პირველი მესამე მეხუთე ელემენტები გამოიყენეთ indexing (მინუს ინდექსებითაც)
-
-# 4)მოცემულია სტრინგი "Information".
-# ამოიღე "forma" ნაწყვეტი slicing-ით (მინუს ინდექსებითაც)
+# 4)
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[0], letters[2], letters[4])   
+print(letters[-7], letters[-5], letters[-3]) 
 
 # 5)
-# მოცემულია სტრინგი "abcdefghijklmno".
-# შექმენი სამი სხვადასხვა სლაისი:
-
-# პირველი შეიცავდეს მხოლოდ a დან d მდე ასოებს
-
-# მეორე – შეიცავდეს j დან o მდე ასოებს
-
-# მესამე – შეიცავდეს f დან j მდე ასოებს
+info = "Information"
+print(info[3:7])     
+print(info[-8:-4])   
+# 6)
+s = "abcdefghijklmno"
+print(s[:4])     
+print(s[9:])    
+print(s[5:10])
