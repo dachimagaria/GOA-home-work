18ebcbc41c89b4857ff40e73775507b60e4ede7b
dachimagaria <dachiarkania023@gmail.com>
2025-10-29 21:24:57 -0700

    1

---
+++ day32/cw/cw.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+for i in range(0, 101):
+    if i == 0:
+        print(i, "is zero")
+    elif i % 2 == 0:
+        print(i, "is even")
+    else:
+        print(i, "is odd")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
