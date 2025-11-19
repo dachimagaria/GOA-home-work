763dc3eccb528be8c8f45cd331d0538de188705c
dachimagaria <dachiarkania023@gmail.com>
2025-11-19 06:43:28 -0800

    1

---
+++ day41/cw/cw.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+# lst = [980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+# lst.append("ianvari")
+# print(lst)
+
+# lst = [980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+# lst.insert(2, "bati")
+# print(lst)
+
+# lst = [980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+# lst.pop(5)
+# print(lst)
+
+# lst = [980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+# lst.remove(True)
+# lst.remove("kote")
+# print(lst)
