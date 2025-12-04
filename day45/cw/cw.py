6a2decf2e54aeb23e50c1d506d9bdc8dbe70ddda
dachimagaria <dachiarkania023@gmail.com>
2025-12-04 06:16:44 -0800

    1

---
+++ day45/cw/cw.py
@@ -0,0 +1,27 @@
+
+
+
+
+name = ["goga", "givia", "giorgi", "nika", "givia", "nika", "saba", "goga", "givia"]
+
+
+
+
+
+name = ["goga", "givia", "giorgi", "nika", "givia", "nika", "saba", "goga", "givia"]
+
+for i in range(len(name)-1, -1, -1):  
+    if len(name[i]) > 5:
+        name.remove(name[i])
+
+print(name)
+
+
+
+
+
+
+
+
+
+
