f56b0ca0e9d007848306566f14137fdd511f38db
dachimagaria <dachiarkania023@gmail.com>
2026-03-10 08:25:58 -0700

    1

--- day72/Credit Card Mask.py
+++
@@ -1,9 +0,0 @@
-def maskify(cc):
-    if len(cc) <= 4:
-        return cc  
-    else: 
-        return "#" * (len(cc) - 4) + cc[-4:] 
-
-print(maskify("4556364607935616"))  
-print(maskify("1234"))              
-print(maskify("123"))               
--- day72/Well of Ideas - Easy Version.py
+++
@@ -1,9 +0,0 @@
-def well(x):
-    count = x.count('good')
-    if count == 0:
-        return "Fail!"
-    elif count <= 2:
-        return "Publish!"
-    else:  
-        return "I smell a series!"
-    
