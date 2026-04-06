23df0454c00e4564ef810931dd464f81b88bfc8a
dachimagaria <dachiarkania023@gmail.com>
2026-04-06 06:01:59 -0700

    1

---
+++ day79/hw/Cat years, Dog years.py
@@ -0,0 +1,18 @@
+def human_years_cat_years_dog_years(human_years):
+    
+    cat_years = 0
+    dog_years = 0
+
+    if human_years >= 1:
+        cat_years = 15
+        dog_years = 15
+
+    if human_years >= 2:
+        cat_years += 9
+        dog_years += 9
+
+    if human_years > 2:
+        cat_years += (human_years - 2) * 4
+        dog_years += (human_years - 2) * 5
+
+    return [human_years, cat_years, dog_years]
